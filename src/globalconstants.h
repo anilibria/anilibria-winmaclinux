371c53f5bcdad7e3b5e51a8535254ae163e9ef8b
Romanus <trueromanus@gmail.com>
2021-10-02 16:24:10 +0300

    * upversion

--- src/globalconstants.h
+++ src/globalconstants.h
@@ -27,7 +27,7 @@ extern QString AnilibriaApiPath;
 
 extern bool IsPortable;
 
-const QString ApplicationVersion = "1.0.3";
+const QString ApplicationVersion = "1.0.4";
 
 const int HistoryReleaseCardMode = 0;
 
