7fc2fa9b6f11d64d4f6966d425132e28d21cff95
Roman Vladimirov <trueromanus@gmail.com>
2021-11-05 21:25:50 +0300

    Update build_rpm.sh

--- specs/build_rpm.sh
+++ specs/build_rpm.sh
@@ -4,13 +4,20 @@
 cd ~/anilibria-winmaclinux
 git pull
 sed -i 's/\/opt\/\$\$[{]TARGET[}]/\/usr\/local/g' AniLibria.pro
+# remove lines in Anilibria.pro
+# else: unix:!android: target.path
+# !isEmpty(target.path) {
+# ..
+# }
+# change line anilibria.desktop
+# Exec=usr/bin/AniLibria
 
 # refresh spec file
 #cp ../spec/anilibria.spec ~/rpmbuild/SPECS
 
 # create tarball
-tar -czvf anilibria-winmaclinux-1.0.3.tar.gz .
-mv anilibria-winmaclinux-1.0.3.tar.gz ~/rpmbuild/SOURCES
+tar -czvf anilibria-winmaclinux-1.0.5.tar.gz .
+mv anilibria-winmaclinux-1.0.5.tar.gz ~/rpmbuild/SOURCES
 
 # build rpm
 cd ~/rpmbuild/SPECS
