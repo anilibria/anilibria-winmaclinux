d26109a31f02462db07978a76507740995699eeb
Roman Vladimirov <trueromanus@gmail.com>
2022-05-16 17:20:31 +0300

    * upversion

--- src/globalconstants.h
+++ src/globalconstants.h
@@ -27,7 +27,7 @@ extern QString AnilibriaApiPath;
 
 extern bool IsPortable;
 
-const QString ApplicationVersion = "1.1.7";
+const QString ApplicationVersion = "1.1.8";
 
 const int HistoryReleaseCardMode = 0;
 
