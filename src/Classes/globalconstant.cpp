c9889e2ed322b84bebacd4dc05cfe6a365914b52
Roman Vladimirov <trueromanus@gmail.com>
2025-02-09 13:55:36 +0300

    * Increase version

--- src/Classes/globalconstant.cpp
+++ src/Classes/globalconstant.cpp
@@ -21,7 +21,7 @@
 QString AnilibriaImagesPath = "https://anilibriaqt.anilib.top";
 QString AnilibriaApiPath = "https://anilibriaqt.anilib.top/";
 bool IsPortable = false;
-QString ApplicationVersion = "2.2.24";
+QString ApplicationVersion = "2.2.25";
 
 int HistoryReleaseCardMode = 0;
 int HistoryWatchReleaseCardMode = 1;
