4f60e8dff3c577d5e999e5f97278eb7aeedfe58c
Roman Vladimirov <trueromanus@gmail.com>
2025-07-03 20:54:48 +0300

    * Rename mail.qml and fixed mpv dependency for mac and linux

--- src/main.cpp
+++ src/main.cpp
@@ -220,7 +220,7 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;    
     engine.rootContext()->setContextProperty("ApplicationVersion", ApplicationVersion);
 
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreated,
