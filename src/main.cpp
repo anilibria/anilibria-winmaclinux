8325f2d675cf68129e9988b0fd8e963f8564af3e
Roman Vladimirov <trueromanus@gmail.com>
2025-09-24 18:04:38 +0300

    Fix loading module

--- src/main.cpp
+++ src/main.cpp
@@ -229,7 +229,7 @@ int main(int argc, char *argv[])
         Qt::QueuedConnection
     );
 
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
     engine.loadFromModule("aniliberty", "Main");
 #else
     engine.load(url);
