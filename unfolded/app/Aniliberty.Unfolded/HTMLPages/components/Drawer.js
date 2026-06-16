028c2b933858f5a3d23a0dd0dcb6198c53c58c2b
Roman Vladimirov <trueromanus@gmail.com>
2026-06-16 17:04:06 +0300

    Move few code to separate files Added page header

---
+++ unfolded/app/Aniliberty.Unfolded/HTMLPages/components/Drawer.js
@@ -0,0 +1,36 @@
+﻿import { ref } from '/static/vue.global.js'
+import { getMainMenu, getUserData } from '/static/mainmenu.js'
+
+export default {
+	props: ['mainMenuVisible'],
+	template: `
+	<div class="main-menu" :style="{'visibility': (mainMenuVisible ? 'visible' : 'hidden'), 'transform': (mainMenuVisible ? 'translateX(0px)': 'translateX(-300px)')}">
+		<div v-if="userData && userData.name" class="main-menu-user">
+			<img :src="userData.avatar" />
+			<span>Вы авторизованы как:<br>{{userData.name}}</span>
+			<img src="/static/icons/mainmenu/logout.svg" width="30" height="30" />
+		</div>
+		<div class="main-menu-items">
+			<div v-for="item in mainMenu">
+				<div class="main-menu-item">
+					<img width="30" height="30" :src="'/static/icons/mainmenu/' + item.icon + '.svg'" />
+					<span>{{item.name}}</span>
+				</div>
+			</div>
+		</div>
+		<div class="main-menu-logo">
+			<div class="icon"></div>
+			<span class="version">Aniliberty.Unfolded<br>версия 0.0.0</span>
+		</div>
+	</div>
+	`,
+	setup(props) {
+		const mainMenu = ref(getMainMenu());
+		const userData = ref(getUserData());
+
+		return {
+			mainMenu,
+			userData
+		};
+	}
+};
---
+++ unfolded/app/Aniliberty.Unfolded/HTMLPages/components/PageHeader.js
@@ -0,0 +1,25 @@
+﻿import { ref } from '/static/vue.global.js'
+
+export default {
+	props: ['mainMenuVisible', 'title'],
+	emits: ['update:mainMenuVisible'],
+	template: `
+		<div class="page-upper-header">
+			<div class="icon-button" @click="$emit('update:mainMenuVisible', mainMenuVisible ? false : true)" title="Открыть меню приложения">
+				<img src="/static/icons/commonbuttons/opendrawer.svg" width="20" height="20" />
+			</div>
+			<div class="page-header">{{title}}</div>
+			<div class="icon-button" title="Полезные ссылки">
+				<img src="/static/icons/commonbuttons/openinformation.svg" width="20" height="20" />
+			</div>
+			<div class="icon-button" title="Вернуться назад в истории переходов" @click="history.back();">
+				<img :src="'/static/icons/commonbuttons/' + (history.length > 1 ? 'navigationback' : 'navigationbackdisabled') + '.svg'" width="20" height="20" />
+			</div>
+		</div>
+	`,
+	setup(props) {
+		return {
+			history
+		};
+	}
+};
