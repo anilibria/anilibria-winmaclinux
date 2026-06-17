81af9ea4b69caac35b0910a215da69ce2bb9c9c5
Roman Vladimirov <trueromanus@gmail.com>
2026-06-17 16:52:21 +0300

    Added select for sections

---
+++ unfolded/app/Aniliberty.Unfolded/HTMLPages/components/SimpleSelect.js
@@ -0,0 +1,22 @@
+﻿import { ref } from '/static/vue.global.js'
+import { getMainMenu, getUserData } from '/static/mainmenu.js'
+
+export default {
+	props: ['item','options'],
+	emits: ['update:item'],
+	template: `
+<div class="simple-select-container">
+	<select v-model="selectValue" @change="$emit('update:item', $event)" class="simple-select">
+		<option v-for="option in options" :value="option.id" :key="option.id">
+			{{option.title}}
+		</option>
+	</select>
+</div>`,
+	setup(props) {
+		const selectValue = ref(props.item);
+
+		return {
+			selectValue
+		};
+	}
+};
