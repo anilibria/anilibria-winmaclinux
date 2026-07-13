0d728110e10b42ffbe6993f94fdbc627523bce1d
Roman Vladimirov <trueromanus@gmail.com>
2026-07-13 15:02:48 +0300

    Implement save preffered quality

--- unfolded/app/Aniliberty.Unfolded/Models/Settings/ApplicationSettingsVideoplayerModel.cs
+++ unfolded/app/Aniliberty.Unfolded/Models/Settings/ApplicationSettingsVideoplayerModel.cs
@@ -3,6 +3,9 @@
 
 	public record ApplicationSettingsVideoplayerModel
 	{
+
+		public IEnumerable<string> PrefferedQuality { get; set; } = ["hls1080", "hls720", "hls480"];
+
 	}
 
 }
--- unfolded/app/Aniliberty.Unfolded/Routes/Settings.cs
+++ unfolded/app/Aniliberty.Unfolded/Routes/Settings.cs
@@ -28,6 +28,7 @@ namespace Aniliberty.Unfolded.Routes
 		{
 			app.MapGet("/settings/bypage", ([FromQuery] string page) => ByPage(page));
 			app.MapPost("/settings/save/releases", ([FromBody] ApplicationSettingsReleasesModel model, CancellationToken cancellationToken) => SaveReleases(model, cancellationToken));
+			app.MapPost("/settings/save/videoplayer", ([FromBody] ApplicationSettingsVideoplayerModel model, CancellationToken cancellationToken) => SaveVideoPlayer(model, cancellationToken));
 		}
 
 		private static IResult ByPage(string page)
@@ -39,6 +40,19 @@ namespace Aniliberty.Unfolded.Routes
 				case "videoplayer": return Results.Json(m_settings.VideoPlayer, AppJsonSerializerContext.Default);
 				default: return Results.NotFound();
 			}
+		}
+
+		private static async Task<IResult> SaveVideoPlayer(ApplicationSettingsVideoplayerModel model, CancellationToken cancellationToken)
+		{
+			if (model == null) return Results.StatusCode(400);
+
+			m_settings.VideoPlayer = model;
+
+			var json = JsonSerializer.Serialize(m_settings, AppJsonSerializerContext.Default.ApplicationSettingsModel);
+			var path = Path.Combine(GlobalConfig.PathToCache(), "settings");
+			await File.WriteAllTextAsync(path, json, cancellationToken);
+
+			return Results.Ok();
 		}
 
 		private static async Task<IResult> SaveReleases(ApplicationSettingsReleasesModel model, CancellationToken cancellationToken)
